06010d38b4d40ee46a734021ea86c0f8600ca47d
AntonGalygo <anton.galygo@mail.ru>
2023-12-15 11:15:31 +0800

    add engineGame fix mistake

--- src/prime-game.js
+++ src/prime-game.js
@@ -2,11 +2,11 @@ import { getNumber, engineGame } from './index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
 
 const getValues = () => {
   const result = [];
-  const question = getNumber(100);
+  const question = getNumber(47);
   const rightAnswer = primeNumbers.includes(question) ? 'yes' : 'no';
   result.push(question, rightAnswer);
   return result;
